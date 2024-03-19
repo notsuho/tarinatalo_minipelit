f7857f5c515e2d333dbcdc2f1f375f3f635f68fd
jokorkko <94602273+jokorkko@users.noreply.github.com>
2024-03-19 16:28:01 +0200

    Kirjahylly UI
    Lisätty Kirjahyllyyn:
    -UI
    -Progress bar
    -Kirjat oikeinpäin
    -Oikein olevan hyllyn kirjat hohtavat
    -Väärin olevan hyllyn kirjat käyvät punaisena
    -Oikein olevan hyllyn kirjat lukittuu
    -Lisätty viive ennen uusien kirjojen tuloa

---
+++ Assets/Kirjahylly/Scripts/UIManager_Kirjahylly.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager_Kirjahylly : MonoBehaviour
+{
+
+    private VisualElement root;
+    
+    private VisualElement panelSection;
+    private Label panelHeadline;
+    private Label panelText;
+    private Button panelButton;
+    private VisualElement instructions;
+    private ProgressBar progressBar;
+
+    private string gotItButtonText = "<allcaps>selvä!</allcaps>";
+    private string instructionHeadlineText = "<allcaps>ohjeet</allcaps>";
+    private string instructionTextText = "Tässä on ohjeet";
+
+    private void OnEnable()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+
+
+        Button instructionButton = root.Q<Button>("instruction-button");
+        Button exitButton = root.Q<Button>("exit-button");
+
+        panelSection = root.Q<VisualElement>("panel-section");
+        panelHeadline = panelSection.Q<Label>("panel-headline");
+        panelText = panelSection.Q<Label>("panel-text");
+        panelButton = panelSection.Q<Button>("panel-button");
+
+        SetInstructions();
+        instructionButton.clicked += () => SetInstructions();
+        panelButton.clicked += () => instructions.style.display = DisplayStyle.None;
+        exitButton.clicked += () => Application.Quit();
+    }
+
+    private void SetInstructions ()
+    {
+        instructions = root.Q<VisualElement>("panel-section");
+        Label instructionHeadline = instructions.Q<Label>("panel-headline");
+        instructionHeadline.text = instructionHeadlineText;
+        Label instructionText = instructions.Q<Label>("panel-text");
+        instructionText.text = instructionTextText;
+        Button gotItButton = instructions.Q<Button>("panel-button");
+        gotItButton.text = gotItButtonText;
+
+        instructions.style.display = DisplayStyle.Flex;
+    }
+
+    public void UpProgressBar(float points, float pointsToWin)
+    {
+        progressBar = root.Q<ProgressBar>("progress-bar");
+        progressBar.value = points;
+        Debug.Log("progress bar value: " + progressBar.value);
+        print(progressBar.value);
+
+        if (progressBar.value >= pointsToWin)
+        {
+            VisualElement star1 = root.Q<VisualElement>("star1");
+            star1.style.backgroundImage = Resources.Load<Texture2D>("Images/star");
+
+            //tähti suurenee ja pienenee    
+            star1.ToggleInClassList("star-scale-transition");
+            root.schedule.Execute(() => star1.ToggleInClassList("star-scale-transition")).StartingIn(500);
+ 
+        }
+
+    }
+    }
