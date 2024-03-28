50c7269651251c681c8c100349a305d207ee09b9
Julkimo <santeri.julkunen@gmail.com>
2024-03-28 14:23:42 +0200

    Further refactoring for JarBehavior. Less GameObject.Find calls needed.

--- Assets/Hillopurkit/Scripts/JarBehavior.cs
+++ Assets/Hillopurkit/Scripts/JarBehavior.cs
@@ -10,17 +10,19 @@ public class JarBehavior : MonoBehaviour
         IsCorrectAnswer = _isCorrectAnswer;
     }
 
-    // Clicking on a correct jar breaks it and starts the next round. Wrong jar only wobbles.
+    // Clicking on a correct jar breaks it and starts the next round. Wrong jar only wobbles. Only works when game is unpaused.
     private void OnMouseDown()
     {
         if (MiniGameManager.isGamePaused)
             return;
 
+        MiniGameManager miniGameManager = GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>();
+
         if (IsCorrectAnswer)
         {
             Vector3 currentPosition = transform.position;
-            GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().BrokeCorrectJar(true); // update score
-            StartCoroutine(GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().NextRound()); // start next round
+            miniGameManager.BrokeCorrectJar(true); // update score
+            StartCoroutine(miniGameManager.NextRound()); // start next round
             
             Instantiate(brokenJar, currentPosition, Quaternion.identity);
             gameObject.transform.position = currentPosition + new Vector3 (30, 0, 0); // move out of view until NextRound() despawns this jar.
@@ -28,7 +30,7 @@ public class JarBehavior : MonoBehaviour
         //
         else
         {
-            GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>().BrokeCorrectJar(false); // update score
+            miniGameManager.BrokeCorrectJar(false); // update score
             GetComponent<Animator>().Play("WrongJar");
         }
     }
