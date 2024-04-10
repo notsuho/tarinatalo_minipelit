015371766564c363e9149977f0994ced347f48d6
Sampsa Jalli <sampsa.jalli@gmail.com>
2024-04-10 16:54:38 +0300

    streakit lisätty, progress bar päivittymistä muokkailtu dynaamisemmaksi

--- Assets/Hillopurkit/UI/HillopurkitUIManager.cs
+++ Assets/Hillopurkit/UI/HillopurkitUIManager.cs
@@ -15,10 +15,11 @@ public class HillopurkitUIManager : MonoBehaviour
     private VisualElement instructions;
     private ProgressBar progressBar;
     private Label tallyText;
-    private Label pointsText;
+    private Label scoreLabel;
     private Label clickedWrong;
     private Label clickedRight;
-
+    private VisualElement streakImage;
+    private const int SCORE_MULTIPLIER = 10; // for UI display purposes
     private readonly string continueButtonText = "<allcaps>jatka</allcaps>";
     private readonly string gotItButtonText = "<allcaps>selvä!</allcaps>";
     private readonly string endGameButtonText = "<allcaps>palaa pääpeliin</allcaps>";
@@ -31,10 +32,7 @@ public class HillopurkitUIManager : MonoBehaviour
     {
         root = GetComponent<UIDocument>().rootVisualElement;
 
-        ResetProgressBar(); // We intentionally reset the progress bar at 1/3 full.
-                            // In case someone is debugging later and wonders
-                            // why it doesn't start at zero. Once we have global
-                            // tracking for score this should be changed back!
+        ResetProgressBar();
 
         Button instructionButton = root.Q<Button>("instruction-button");
         Button exitButton = root.Q<Button>("exit-button");
@@ -56,7 +54,8 @@ public class HillopurkitUIManager : MonoBehaviour
 
     private void SetInstructions()
     {
-        string instructionTextText = "Kaappiin on kasattu purkkeja, joiden kyljessä lukee synonyymejä. " //Siirrä tiedostoon myöhemmin
+        //Siirrä tiedostoon myöhemmin
+        string instructionTextText = "Kaappiin on kasattu purkkeja, joiden kyljessä lukee synonyymejä. " 
                                     + "Mutta purkkien joukkoon on eksynyt sana, joka ei kuulu joukkoon. "
                                     + "Etsi joukoon kuulumaton purkki, ja riko se vasaralla!";
 
@@ -99,6 +98,9 @@ public class HillopurkitUIManager : MonoBehaviour
 
     public IEnumerator DeclareWin()
     {
+        // Update the score for next minigame to use
+        GameManager.totalPoints = score.GetPoints();
+
         yield return new WaitForSeconds(WaitTimes.MESSAGE_TIME_LONG);
 
         int[] tally = score.GetTally();
@@ -106,7 +108,8 @@ public class HillopurkitUIManager : MonoBehaviour
 
         panelHeadline.text = winningHeadline;
         panelText.text = winningText
-            + ("\n\nArvausten määrä: " + total) // Add two linebreaks so it looks just a tiny bit cleaner
+            + ("\n\nPisteesi: " + score.GetPoints()) // Add two linebreaks so it looks just a tiny bit cleaner
+            + ("\nArvausten määrä: " + total)
             + ("\nOikeat arvaukset: " + tally[0])
             + ("\nVäärät arvaukset: " + tally[1]);
 
@@ -121,64 +124,123 @@ public class HillopurkitUIManager : MonoBehaviour
         };
     }
 
-    public void UpProgressBar(float currentPoints)
+    public void UpProgressBar(int currentPoints)
     {
+        VisualElement star1 = root.Q<VisualElement>("star1");
+        VisualElement star2 = root.Q<VisualElement>("star2");
+        VisualElement star3 = root.Q<VisualElement>("star3");
+
         progressBar = root.Q<ProgressBar>("progress-bar");
         Debug.Log("\nProgress bar value before update: " + progressBar.value);
         progressBar.value = currentPoints;
         Debug.Log("\nProgress bar value after update: " + progressBar.value);
 
-        if (progressBar.value >= 33f)
+        if (progressBar.value < 33)
         {
-            VisualElement star1 = root.Q<VisualElement>("star1");
+            UnlightStar(star1);
+            UnlightStar(star2);
+            UnlightStar(star3);
+        }
+
+        if (progressBar.value >= 33)
+        {
+            //VisualElement star1 = root.Q<VisualElement>("star1");
             star1.style.backgroundImage = Resources.Load<Texture2D>("Images/star");
+
+            if (progressBar.value >= 33 && progressBar.value <= 40) {
+            StarScaleTransition(star1);
+            }
+            //star1.ToggleInClassList("star-scale-transition");
+            //root.schedule.Execute(() => star1.ToggleInClassList("star-scale-transition")).StartingIn(500);
         }
 
-        if (progressBar.value >= 66f)
+        if (progressBar.value < 66)
         {
-            VisualElement star2 = root.Q<VisualElement>("star2");
+            UnlightStar(star2);
+            UnlightStar(star3);
+        }
+
+        if (progressBar.value >= 66)
+        {
+            //VisualElement star2 = root.Q<VisualElement>("star2");
             star2.style.backgroundImage = Resources.Load<Texture2D>("Images/star");
+
+            if (progressBar.value >= 66 && progressBar.value <= 70) {
+            StarScaleTransition(star2);
+            }
+            //star2.ToggleInClassList("star-scale-transition");
+            //root.schedule.Execute(() => star2.ToggleInClassList("star-scale-transition")).StartingIn(500);
         }
 
-        if (progressBar.value >= 99f)
+        if (progressBar.value < 99)
         {
-            VisualElement star3 = root.Q<VisualElement>("star3");
-            star3.style.backgroundImage = Resources.Load<Texture2D>("Images/star");
+            UnlightStar(star3);
         }
+
+        if (progressBar.value >= 99)
+        {
+            //VisualElement star3 = root.Q<VisualElement>("star3");
+            star3.style.backgroundImage = Resources.Load<Texture2D>("Images/star");
+
+            StarScaleTransition(star3);
+            //star3.ToggleInClassList("star-scale-transition");
+            //root.schedule.Execute(() => star3.ToggleInClassList("star-scale-transition")).StartingIn(500);
+        }
+
+    }
+
+    private void StarScaleTransition(VisualElement star)
+    {
+        // Twinkle the star when it's filled
+        star.ToggleInClassList("star-scale-transition");
+        root.schedule.Execute(() => star.ToggleInClassList("star-scale-transition")).StartingIn(500);
+    }
+
+    private void UnlightStar(VisualElement star)
+    {
+        // Make star go back to blank from yellow
+        star.style.backgroundImage = Resources.Load<Texture2D>("Images/star_blank");
     }
 
     public void ResetProgressBar()
     {
         progressBar = root.Q<ProgressBar>("progress-bar");
-        // Get the score for reset, in this case it's 33 not 0!
-        progressBar.value = GameObject.Find("Score").GetComponent<Score>().GetPoints(); 
-        VisualElement star1 = root.Q<VisualElement>("star1");
-        star1.style.backgroundImage = Resources.Load<Texture2D>("Images/star"); // light up the first star
-        VisualElement star2 = root.Q<VisualElement>("star2");
-        star2.style.backgroundImage = Resources.Load<Texture2D>("Images/star_blank");
-        VisualElement star3 = root.Q<VisualElement>("star3");
-        star3.style.backgroundImage = Resources.Load<Texture2D>("Images/star_blank");
+        // progressBar.value = GameManager.totalPoints;
+        UpProgressBar(score.GetPoints());
+        // UpProgressBar(GameManager.totalPoints);
+        // progressBar.value = GameObject.Find("Score").GetComponent<Score>().GetPoints(); 
+        // VisualElement star1 = root.Q<VisualElement>("star1");
+        // star1.style.backgroundImage = Resources.Load<Texture2D>("Images/star"); // light up the first star
+        // VisualElement star2 = root.Q<VisualElement>("star2");
+        // star2.style.backgroundImage = Resources.Load<Texture2D>("Images/star_blank");
+        // VisualElement star3 = root.Q<VisualElement>("star3");
+        // star3.style.backgroundImage = Resources.Load<Texture2D>("Images/star_blank");
     }
 
     public void SetFeedback(bool result)
     {
         int[] tally = score.GetTally();
-        float points = score.GetPoints();
+        int points = score.GetPoints();
 
         if (result == true)
         {
-            tallyText = root.Q<Label>("click-tally-right");
-            tallyText.text = ("Särjetyt purkit: " + tally[0]);
-            pointsText = root.Q<Label>("score-label");
-            pointsText.text = ("" + points);
+            //tallyText = root.Q<Label>("click-tally-right");
+            //tallyText.text = ("Särjetyt purkit: " + tally[0]);
+            if (score.streak >= score.minStreakValue) {
+                DisplayStreakImage();
+            }
+            scoreLabel = root.Q<Label>("score-label");
+            scoreLabel.text = ("" + points);
             clickedRight.visible = true;
             StartCoroutine(FeedbackTurnOffDelay(clickedRight));
         }
 
         else
         {
-            tallyText = root.Q<Label>("click-tally-wrong");
-            tallyText.text = ("Väärät arvaukset: " + tally[1]);
+            //tallyText = root.Q<Label>("click-tally-wrong");
+            //tallyText.text = ("Väärät arvaukset: " + tally[1]);
+            scoreLabel = root.Q<Label>("score-label");
+            scoreLabel.text = ("" + points);
             clickedWrong.visible = true;
             StartCoroutine(FeedbackTurnOffDelay(clickedWrong));
         }
@@ -188,5 +250,27 @@ public class HillopurkitUIManager : MonoBehaviour
     {
         yield return new WaitForSeconds(WaitTimes.MESSAGE_TIME_SHORT);
         feedbackMsg.visible = false;
+    }
+
+    //asettaa streak imagen käymään näkyvissä
+    private void DisplayStreakImage ()
+    {       
+            streakImage = root.Q<VisualElement>("streak-image");
+
+            //asettaa kuvaan oikean streakin arvon
+            Label streakCount = streakImage.Q<Label>("streak-count");
+            streakCount.text = "+" + ScoreArkku.streak;
+
+            streakImage.style.display = DisplayStyle.Flex;
+            streakImage.ToggleInClassList("streak-image-transition");
+            Invoke("ToggleStreakClassList", 3f);
+    }
+
+
+    //hävittää streak imagen näkyvistä ja asettaa classlistin alkuperäiseen asentoon
+    private void ToggleStreakClassList()
+    {
+        streakImage.ClearClassList();
+        streakImage.style.display = DisplayStyle.None;
     }
 }
