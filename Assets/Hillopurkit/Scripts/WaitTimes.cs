648673b22188ccda4ac7bd9f2ffbfb6dbe6d548a
Sampsa Jalli <sampsa.jalli@gmail.com>
2024-04-21 14:55:17 +0300

    säädetty oikein/väärin palautetekstin viivettä 0.5->0.8

--- Assets/Hillopurkit/Scripts/WaitTimes.cs
+++ Assets/Hillopurkit/Scripts/WaitTimes.cs
@@ -5,6 +5,6 @@ public static class WaitTimes
     public const float DOOR_OPENING_TIME = 1.0f;
     public const float DOOR_CLOSING_TIME = 0.5f;
     public const float CABINET_SHAKING_TIME = 0.75f;
-    public const float MESSAGE_TIME_SHORT = 0.5f;
+    public const float MESSAGE_TIME_SHORT = 0.8f;
     public const float MESSAGE_TIME_LONG = 1.25f;
 }
