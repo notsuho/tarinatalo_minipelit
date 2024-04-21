cb7ed455d52265da7f678c9ac451d69f10a00e96
Sampsa Jalli <sampsa.jalli@gmail.com>
2024-04-21 14:45:25 +0300

    lisätty TextMaterial luokka hillopurkeille, josta UI elementtien tekstit otetaan

---
+++ Assets/Hillopurkit/Scripts/TextMaterialHillopurkit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextMaterialHillopurkit
+{
+
+    public const string continueButtonText = "<allcaps>jatka</allcaps>";
+    public const string gotItButtonText = "<allcaps>selvä!</allcaps>";
+    public const string endGameButtonText = "<allcaps>palaa pääpeliin</allcaps>";
+    public const string nextGameButtonText = "<allcaps>seuraava peli</allcaps>";
+    public const string instructionHeadlineText = "<allcaps>2/3 lasipurkit\nohjeet</allcaps>";
+    public const string winningHeadline = "Läpäisit pelin!";
+    public const string winningText = "Löysit ja rikoit kaikki joukkoon kuulumattomat purkit!";
+    public const string instructionText = "Kaappiin on kasattu purkkeja, joiden kyljessä lukee synonyymejä. " 
+                                            + "Mutta purkkien joukkoon on eksynyt sana, joka ei kuulu joukkoon. "
+                                            + "Etsi joukkoon kuulumaton purkki, ja riko se vasaralla!";
+
+}
